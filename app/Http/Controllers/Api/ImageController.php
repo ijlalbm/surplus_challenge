00d073afce7afa1ac38d5b2225046d9fc08319e8
ijlalbm <muhammadijlalm@gmail.com>
2023-03-11 11:48:30 +0800

    add feature update image

--- app/Http/Controllers/Api/ImageController.php
+++ app/Http/Controllers/Api/ImageController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\Image;
 use App\Http\Resources\ImageResource;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Storage;
 
 class ImageController extends Controller
 {
@@ -53,5 +54,52 @@ class ImageController extends Controller
 
         //return single image as a resource
         return new ImageResource(true, 'Detail Data Image', $image);
+    }
+
+    public function update(Request $request, $id)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'name'              => 'required',
+            'file'              => 'required|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
+            'enable'            => 'required|boolean',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //find post by ID
+        $image = Image::find($id);
+
+        //check if image is not empty
+        if ($request->hasFile('file')) {
+
+            //upload image
+            $imageFile = $request->file('file');
+            $imageFile->storeAs('public/file/', $imageFile->hashName());
+
+            //delete old image
+            Storage::delete('public/file/'.$image->file);
+
+            //update image with new image
+            $image->update([
+                'name'     => $request->name,
+                'file'     => $imageFile->hashName(),
+                'enable'   => $request->enable,
+            ]);
+
+        } else {
+
+            //update image without image
+            $image->update([
+                'name'     => $request->name,
+                'enable'   => $request->enable,
+            ]);
+        }
+
+        //return response
+        return new ImageResource(true, 'Data Image Berhasil Diubah!', $image);
     }
 }
