4f3041710ddc24bb6e359139eb77429b92aa91bc
ijlalbm <muhammadijlalm@gmail.com>
2023-03-11 21:04:38 +0800

    add seeder

---
+++ database/seeders/ImageSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class ImageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('images')->insert([
+            'name' => 'Laravel Book',
+            'file' => 'laravel.jpg',
+            'enable' => 1,
+        ]);
+    }
+}
