ff908cca2277552c86f7127a47db7f585161a07e
ijlalbm <muhammadijlalm@gmail.com>
2023-03-11 09:55:14 +0800

    add feature update product

--- app/Http/Controllers/Api/ProductController.php
+++ app/Http/Controllers/Api/ProductController.php
@@ -50,4 +50,44 @@ class ProductController extends Controller
         //return single product as a resource
         return new ProductResource(true, 'Detail Data Product', $product);
     }
+
+    public function update(Request $request, $id)
+    {
+        //define validation rules
+        $validator = Validator::make($request->all(), [
+            'name'              => 'required',
+            'description'       => 'required',
+            'enable'            => 'required|boolean',
+        ]);
+
+        //check if validation fails
+        if ($validator->fails()) {
+            return response()->json($validator->errors(), 422);
+        }
+
+        //find product by ID
+        $product = Product::find($id);
+
+        $product->update([
+            'name'              => $request->name,
+            'description'       => $request->description,
+            'enable'            => $request->enable,
+        ]);
+
+        //return response
+        return new ProductResource(true, 'Data Product Berhasil Diupdate!', $product);
+    }
+
+    public function destroy($id)
+    {
+
+        //find product by ID
+        $product = Product::find($id);
+
+        //delete product
+        $product->delete();
+
+        //return response
+        return new ProductResource(true, 'Data product Berhasil Dihapus!', null);
+    }
 }
